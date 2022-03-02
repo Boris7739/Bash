18691eae55abc78d16912d55bc8d2952fc04682c
Boris <ic3peak7@mail.ru>
2022-03-02 13:15:50 +0400

    add coments

--- alerting_for_logs.sh
+++ alerting_for_logs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-path0='/home/user/server.log'
-count=$(cat count_lenght.txt)
-log=$(wc -l < $path0)
-def="$(($log - $count))"
+path0='/home/user/server.log' # Лог файл который нужно мониторить
+count=$(cat count_lenght.txt) # Нужно до скрипта создать файл count_lenght.txt - он будет хранить только пердыдущее(до запуска скрипта) значение числа строчек в файле server.log. count_lenght.txt - изначально содержит 0 строчек 
+log=$(wc -l < $path0) # забираем число строчек
+def="$(($log - $count))" # Минусуем от старого числа из count_lenght.txt
 echo "New row in log: $def"
-echo "$log" > count_lenght.txt
-cat ${path0} | tail -$def | grep 'Error'
-mail=$(cat ${path0} | tail -$def | grep 'Error' | wc -l)
+echo "$log" > count_lenght.txt # Перезаписываем новое число в count_lenght.txt
+cat ${path0} | tail -$def | grep 'Error' # Показываем новые строчки из server.log (фильтруем по ошибкам)
+mail=$(cat ${path0} | tail -$def | grep 'Error' | wc -l) # Записываем число новых ошибок в переменную
 echo "$mail"
 
 
